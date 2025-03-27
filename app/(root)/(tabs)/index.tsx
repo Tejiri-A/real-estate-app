3c52b60bcffc92ad41d8febbf96b2f3526d838a4
Oghenetejiri Samuel Amrasa <tjthecreator8@gmail.com>
2025-03-27 18:48:48 +0100

    feat(tabs): implement tab navigation layout with icons for Home, Explore, and Profile

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -11,10 +11,10 @@ export default function Index() {
       }}
     >
       <Text className="my-10 font-rubik text-3xl">Welcome to ReState</Text>
-      <Link href="/sign-in">Sign In</Link>
+      {/* <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
-      <Link href="/properties/1">Property details</Link>
+      <Link href="/properties/1">Property details</Link> */}
     </View>
   );
 }
